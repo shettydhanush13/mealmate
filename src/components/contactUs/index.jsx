64f229ff49bb9a19c55d873cda7b159367cb1c23
Dhanush.shetty <dhanush.shetty@cimpress.com>
2024-11-25 11:01:15 +0530

    ui for release

--- src/components/contactUs/index.jsx
+++ src/components/contactUs/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
 import TextField from "@mui/material/TextField";
 import './styles.scss';
 
