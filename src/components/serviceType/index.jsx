149deac0da781a458ea3b4e2297082471f0a2efb
Dhanush.shetty <dhanush.shetty@cimpress.com>
2024-12-23 17:30:56 +0530

    remove banner description

--- src/components/serviceType/index.jsx
+++ src/components/serviceType/index.jsx
@@ -4,14 +4,14 @@ import { useNavigate } from 'react-router-dom';
 
 const ServiceType = ({ service }) => {
     
-  const { banner, link, description, title } = service;
+  const { banner, link } = service;
   const navigate = useNavigate();
 
   return <div className="serviceTypeContainer" onClick={() => navigate(link)}>
     <img src={banner} alt="" />
-    <div className="description">
+    {/* <div className="description">
       <p><b>{title}</b> - {description}</p>
-    </div>
+    </div> */}
   </div>
 };
 
