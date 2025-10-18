a0383a814e91f5cab6d7a19541763e4b6d4423fe
Dhanush.shetty <dhanush.shetty@cimpress.com>
2025-10-18 19:42:01 +0530

    update base url

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-const api_base = 'http://localhost:3001';
-//const api_base = 'https://caterkart-api.onrender.com'
+// const api_base = 'http://localhost:3001';
+const api_base = 'https://caterkart-api.onrender.com'
 
 export const verify_api_base = `${api_base}/verify`
 export const order_api_base = `${api_base}/order`
